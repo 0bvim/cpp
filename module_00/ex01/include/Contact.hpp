05aadac71d5730724204ec0466d46e51c79fa96b
vde-frei <vde-frei@student.42sp.org.br>
2024-04-01 22:03:54 -0300

    start to insert getters and setters in contact

--- module_00/ex01/include/Contact.hpp
+++ module_00/ex01/include/Contact.hpp
@@ -3,4 +3,21 @@
 
 #include <string>
 
+class Contact {
+private:
+	std::string _firstName;
+	std::string _lastName;
+	std::string _nickName;
+	std::string _darkestSecret;
+	std::string _phone;
+
+
+public:
+	Contact();
+
+	~Contact();
+	std::string getFirstName();
+	void setFirstName(const std::string&_firstName);
+};
+
 #endif // !CONTACT_HPP
