f5d2cfb7e7dd3720c938125aaa241a0badc798dc
vde-frei <vde-frei@student.42sp.org.br>
2024-04-03 21:00:03 -0300

    fix: name of classes in constructor and in destructor

--- module_00/ex01/include/PhoneBook.hpp
+++ module_00/ex01/include/PhoneBook.hpp
@@ -3,4 +3,15 @@
 
 #include "Contact.hpp"
 
+class PhoneBook {
+
+private:
+	int	_contacts[8];
+
+public:
+	PhoneBook(void);
+	~PhoneBook(void);
+};
+
+
 #endif // !PHONEBOOK_HPP
---
+++ module_00/ex01/src/PhoneBook.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(void) {
+	std::cout << "PhoneBook Constructor Called" << std::endl;
+}
+
+PhoneBook::~PhoneBook(void) {
+	std::cout << "PhoneBook Destructor Called" << std::endl;
+}
