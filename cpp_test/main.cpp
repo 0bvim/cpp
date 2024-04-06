95039d092bb0e1d2cad3c6e8d6d1b58f89f46151
vimni <niviciusdev@gmail.com>
2024-04-06 20:20:00 -0300

    feat: updated comments

--- cpp_test/main.cpp
+++ cpp_test/main.cpp
@@ -4,7 +4,9 @@
 int main (void) {
 	Sample instance;
 
+	/* adding value in a variable using object instance */
 	instance.setFoo(42);
+	/* with get, you can get value attributed to a variable of object */
 	std::cout << "Foo value after set: " << instance.getFoo() << std::endl;
 	instance.setFoo(-42);
 	std::cout << "Foo value after try to set a negative value: " << instance.getFoo() << std::endl;
