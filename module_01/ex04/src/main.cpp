dc27a4a171a340762a87fb7e8fab468a57efd7a1
vimni <niviciusdev@gmail.com>
2024-04-14 12:20:25 -0300

    feat: implementing function
    I need to stop for lunch

--- module_01/ex04/src/main.cpp
+++ module_01/ex04/src/main.cpp
@@ -1,5 +1,18 @@
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-int main(void) {
-  return EXIT_SUCCESS;
+void replaceString(std::string file, std::string old, std::string new) {
+	if (std::ifstream(file))
 }
+int main (int argc, char *argv[]) {
+	(void)argv;
+	if (argc != 4) {
+		std::cerr << "3 arguments needed: <file> <old string> <new string2>" << std::endl;
+	} else {
+		std::cout << "Correct arguments" << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
+
