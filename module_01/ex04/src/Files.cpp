a5456824ff1e45f73514ad3b09b73eeaf832e2b4
vde-frei <vde-frei@student.42sp.org.br>
2024-04-14 22:56:28 -0300

    feat: getter and setter

--- module_01/ex04/src/Files.cpp
+++ module_01/ex04/src/Files.cpp
@@ -1,3 +1,14 @@
 #include "Files.hpp"
 
 Files::Files(){}
+
+Files::~Files(){}
+
+void Files::setFileName(std::string const name) {
+	this->_fileName = name;
+}
+
+std::string Files::getFileName() {
+	return this->_fileName;
+}
+
