162352b5567950bf9d0e70790dc743b55a81f9b9
nivicius <niviciusdev@gmail.com>
2024-04-18 20:36:52 -0300

    feat: implementing canonical form in fixed class
    but now I need to go to dinner before my wife kill me..

--- module_02/ex00/include/Fixed.hpp
+++ module_02/ex00/include/Fixed.hpp
@@ -1,11 +1,18 @@
-#pragma once
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
 class Fixed {
 public:
   Fixed(void);
+  Fixed(int const n);
+  Fixed(Fixed const &src);
+  Fixed &operator=(Fixed const &rhs);
+
   ~Fixed(void);
+
+private:
+  int _fixedValue;
+  static const int raw;
 };
 
 #endif // FIXED_HPP
