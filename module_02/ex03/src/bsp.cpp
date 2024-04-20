771d231f58307759f3fed0fd648f919e9b1e80c5
vde-frei <vde-frei@student.42sp.org.br>
2024-04-20 04:00:39 -0300

    fix: calculation function

--- module_02/ex03/src/bsp.cpp
+++ module_02/ex03/src/bsp.cpp
@@ -6,20 +6,19 @@ static Fixed abs(Fixed x) {
   return x;
 }
 
-static Fixed triangleArea(Point const p1, Point const p2, Point const p3) {
-  return (abs((p1.getX() * (p2.getY() - p3.getY()) +
-               (p2.getX() * (p3.getY() - p1.getY())) +
-               (p3.getX() * (p1.getY() - p2.getY()))) /
-              (float)2.0));
+static Fixed triangleArea(Point const A, Point const B, Point const C) {
+  return (abs((A.getX() * (B.getY() - C.getY()) +
+							B.getX() * (C.getY() - A.getY()) +
+              C.getX() * (A.getY() - B.getY())) / (float)2.0));
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
   Fixed abc = triangleArea(a, b, c);
-  Fixed pab = triangleArea(point, a, b);
   Fixed pbc = triangleArea(point, b, c);
-  Fixed pca = triangleArea(point, c, a);
+  Fixed pac = triangleArea(a, point, c);
+  Fixed pab = triangleArea(a, b, point);
 
-  if (pab == 0 || pbc == 0 || pca == 0)
+  if (pab == 0 || pbc == 0 || pac == 0)
     return false;
-  return (pab == pbc) && (pbc == pca);
+  return (abc == (pbc + pac + pab));
 }
--- module_02/ex03/src/main.cpp
+++ module_02/ex03/src/main.cpp
@@ -5,7 +5,7 @@
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main(void) {
-  if (bsp(Point(0, 0), Point(20, 0), Point(10, 30), Point(10, 15)))
+  if (bsp(Point(0, 0), Point(10, 30), Point(20, 0), Point(10, 15)))
     std::cout << "Point is in the triangle" << std::endl;
   else
     std::cout << "Point is not in the triangle" << std::endl;
