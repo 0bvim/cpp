7bdcebcfaef60aa5e285b9861eb7f7cb9743c788
vde-frei <vde-frei@student.42sp.org.br>
2024-04-20 01:26:15 -0300

    feat: fix point cpp

--- module_02/ex03/src/Point.cpp
+++ module_02/ex03/src/Point.cpp
@@ -1,26 +1,19 @@
 #include "Point.hpp"
 
-Point::Point(void) : _x(0), _y(0) {};
+Point::Point(void) : _x(0), _y(0){};
 
-Point::Point(const float x, const float y) : _x(x), _y(y) {};
+Point::Point(const float x, const float y) : _x(x), _y(y){};
 
-Point::Point(const Point &to_copy) {
-	*this = to_copy;
-};
+Point::Point(const Point &src) { *this = src; };
 
-Point& Point::operator=(const Point &original)
-{
-	(Fixed)this->_x = original.getX();
-	(Fixed)this->_y = original.getY();
-	return *this;
+Point &Point::operator=(const Point &rhs) {
+  (Fixed) this->_x = rhs.getX();
+  (Fixed) this->_y = rhs.getY();
+  return *this;
 }
 
-Point::~Point(void) {};
+Point::~Point(void){};
 
-Fixed Point::getX(void) const {
-	return this->_x;
-}
+Fixed Point::getX(void) const { return this->_x; }
 
-Fixed Point::getY(void) const {
-	return this->_y;
-}
+Fixed Point::getY(void) const { return this->_y; }
