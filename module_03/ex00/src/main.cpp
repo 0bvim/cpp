90a13cbfef3ac75a5142d68ba354e92d402cfdb5
vde-frei <vde-frei@student.42sp.org.br>
2024-04-20 04:05:39 -0300

    fixed main file

--- module_03/ex00/src/main.cpp
+++ module_03/ex00/src/main.cpp
@@ -1,17 +1,8 @@
-#include "Fixed.hpp"
 #include <cstdlib>
 #include <iostream>
 
 int main(void) {
-  Fixed a;
-  Fixed b(a);
-  Fixed c;
-
-  c = b;
-
-  std::cout << a.getRawBits() << std::endl;
-  std::cout << b.getRawBits() << std::endl;
-  std::cout << c.getRawBits() << std::endl;
+  std::cout << "I need to rest =p" << std::endl;
 
   return EXIT_SUCCESS;
 }
