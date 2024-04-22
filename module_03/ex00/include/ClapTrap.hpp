18ea59972dbf368f04c17b92de9b421916391df2
nivicius <niviciusdev@gmail.com>
2024-04-22 19:50:03 -0300

    feat: implemented full header and canonical

--- module_03/ex00/include/ClapTrap.hpp
+++ module_03/ex00/include/ClapTrap.hpp
@@ -6,16 +6,27 @@
 class ClapTrap {
 public:
   ClapTrap(void);
+	ClapTrap(std::string name);
+	ClapTrap(ClapTrap const &src);
+	ClapTrap &operator=(ClapTrap const &rhs);
   ~ClapTrap(void);
 
   void attack(const std::string &target);
   void takeDamage(unsigned int amount);
   void beRepaired(unsigned int amount);
 
+	unsigned int getHitPoints(void);
+	unsigned int getEnergypoint(void);
+	unsigned int getAttackDamage(void);
+	void setHitPoints(unsigned int);
+	void setEnergypoint(unsigned int);
+	void setAttackDamage(unsigned int);
+
 private:
-  unsigned int energyPoints;
-  unsigned int hitPoints;
-  unsigned int attackDamage;
+	std::string _name;
+  unsigned int _energyPoints;
+  unsigned int _hitPoints;
+  unsigned int _attackDamage;
 };
 
 #endif // !CLAPTRAP_HPP
--- module_03/ex00/src/ClapTrap.cpp
+++ module_03/ex00/src/ClapTrap.cpp
@@ -3,4 +3,27 @@
 #include <iostream>
 
 ClapTrap::ClapTrap(void) { std::cout << "Constructor Called." << std::endl; }
+
+ClapTrap::ClapTrap(std::string name)
+    : _name(name), _energyPoints(10), _hitPoints(10), _attackDamage(0) {
+  std::cout << "Constructor Called." << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap const &src) {
+	std::cout << "Copy Constructor Called" << std::endl;
+	*this = src;
+}
+
+ClapTrap &ClapTrap::operator=(ClapTrap const &rhs) {
+  std::cout << "Assignment operator called" << std::endl;
+	if (this != &rhs) {
+		this->_name = rhs._name;
+		this->_energyPoints = rhs._energyPoints;
+		this->_hitPoints = rhs._hitPoints;
+		this->_attackDamage = rhs._attackDamage;
+	}
+	return *this;
+}
+
 ClapTrap::~ClapTrap(void) { std::cout << "Destructor Called." << std::endl; }
+
