3ef8e4f6f73046d4afe7ab896687b41d7ec750f8
vde-frei <vde-frei@student.42sp.org.br>
2024-04-24 19:50:07 -0300

    fix: identation and improved tests

--- module_02/ex03/src/main.cpp
+++ module_02/ex03/src/main.cpp
@@ -4,10 +4,23 @@
 
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
-int main(void) {
+void test1(void) {
   if (bsp(Point(0, 0), Point(10, 30), Point(20, 0), Point(10, 15)))
     std::cout << "Point is in the triangle" << std::endl;
   else
     std::cout << "Point is not in the triangle" << std::endl;
+}
+
+void test2(void) {
+  if (bsp(Point(0, 0), Point(10, 30), Point(20, 0), Point(30, 15)))
+    std::cout << "Point is in the triangle" << std::endl;
+  else
+    std::cout << "Point is not in the triangle" << std::endl;
+}
+
+int main(void) {
+  test1();
+  test2();
+
   return EXIT_SUCCESS;
 }
