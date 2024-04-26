15f9c4fb1d78e4249d4f824ba990207cce6b9400
vimni <niviciusdev@gmail.com>
2024-04-26 09:39:03 -0300

    doxygem ex01 includes

--- module_03/ex01/include/ScavTrap.hpp
+++ module_03/ex01/include/ScavTrap.hpp
@@ -5,18 +5,22 @@
 
 class ScavTrap : public ClapTrap {
 public:
+  // Enum to define different status types
   enum status { DEAD, ENERGY, DAMAGE, NEG_DMG, EMPTY_ATK, NEG_AMOUNT };
 
-  ScavTrap(void);
-	ScavTrap(const std::string name);
-  ~ScavTrap(void);
-  ScavTrap(ScavTrap const &src);
-  ScavTrap &operator=(ScavTrap const &rhs);
-	
-  void attack(const std::string &target);
-	void guardGate();
+  // Constructors and destructor
+  ScavTrap(void); // Default constructor
+  ScavTrap(const std::string name); // Parameterized constructor
+  ~ScavTrap(void); // Destructor
+  ScavTrap(ScavTrap const &src); // Copy constructor
+  ScavTrap &operator=(ScavTrap const &rhs); // Assignment operator
+
+  // Member functions
+  void attack(const std::string &target); // Function to perform attack
+  void guardGate(); // Function for guarding the gate
 
 private:
+  // Helper function to print messages based on status
   void messages(status type) const;
 };
 
