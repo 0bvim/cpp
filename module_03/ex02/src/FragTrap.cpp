cfbd4396f2e4b7c670e17c7223f15f49c3bbf731
vimni <niviciusdev@gmail.com>
2024-04-26 16:44:03 -0300

    feat: implemented fragtrap cpp functions

--- module_03/ex02/src/FragTrap.cpp
+++ module_03/ex02/src/FragTrap.cpp
@@ -1,1 +1,55 @@
 #include "FragTrap.hpp"
+#include <iostream>
+
+FragTrap::FragTrap(void) : ClapTrap() {
+  std::cout << "FragTrap Constructor Called." << std::endl;
+  this->setHitPoints(100);
+  this->setEnergyPoint(100);
+  this->setAttackDamage(30);
+}
+
+FragTrap::FragTrap(const std::string name) : ClapTrap(name) {
+  std::cout << "FragTrap Constuctor Called." << std::endl;
+  this->setHitPoints(100);
+  this->setEnergyPoint(100);
+  this->setAttackDamage(30);
+}
+
+FragTrap::~FragTrap(void) {
+  std::cout << "FragTrap Destructor Called." << std::endl;
+}
+
+FragTrap::FragTrap(FragTrap const &src) : ClapTrap(src) {
+  std::cout << "FragTrap copy constructor called." << std::endl;
+  *this = src;
+}
+
+FragTrap &FragTrap::operator=(FragTrap const &rhs) {
+  std::cout << "FragTrap assignment constructor called." << std::endl;
+  if (this != &rhs) {
+    this->setName(rhs.getName());
+    this->setHitPoints(rhs.getHitPoints());
+    this->setEnergyPoint(rhs.getEnergypoint());
+    this->setAttackDamage(rhs.getAttackDamage());
+  }
+  return *this;
+}
+
+// Member functions
+void FragTrap::attack(const std::string &target) {
+  if (!getEnergypoint()) {
+    std::cout << "FragTrap " << getName() << " is out of energy." << std::endl;
+    return;
+  }
+  if (target.empty()) {
+    messages(ClapTrap::EMPTY_ATK);
+    return;
+  }
+  std::cout << "FragTrap " << getName() << " attacks " << target << ", causing "
+            << getAttackDamage() << " points of damage!" << std::endl;
+  setEnergyPoint(this->getEnergypoint() - 1);
+}
+
+void FragTrap::highFivesGuys(void) {
+  std::cout << "High five request has been accepted succefully!" << std::endl;
+}
--- module_03/ex02/src/main.cpp
+++ module_03/ex02/src/main.cpp
@@ -1,51 +1,54 @@
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 #include <cstdlib>
 #include <iostream>
 
 int main(void) {
 	{
-		ScavTrap blop("anormal");
+		FragTrap blep("anormal");
 
 		std::cout << "Messages tests" << std::endl;
-		blop.attack("You");
-		blop.attack("");
-		blop.takeDamage(-1);
-		blop.beRepaired(-1);
+		std::cout << blep.getName();
+		blep.attack("You");
+		blep.attack("");
+		blep.takeDamage(-1);
+		blep.beRepaired(-1);
 
 		std::cout << std::endl;
 		std::cout << "Health Points tests" << std::endl;
 
-		blop.takeDamage(1);
-		blop.beRepaired(2);
-		blop.takeDamage(11);
-		blop.takeDamage(1);
+		blep.takeDamage(1);
+		blep.beRepaired(2);
+		blep.takeDamage(11);
+		blep.takeDamage(1);
 
 		std::cout << std::endl;
 		std::cout << "Energy tests" << std::endl;
 
 		/* setuping for energy tests */
-		blop.setEnergyPoint(0);
-		blop.setHitPoints(10);
+		blep.setEnergyPoint(0);
+		blep.setHitPoints(10);
 
-		blop.attack("You again");
-		blop.beRepaired(1);
-		blop.attack("A ghost");
-		blop.guardGate();
+		blep.attack("You again");
+		blep.beRepaired(1);
+		blep.attack("A ghost");
+		blep.highFivesGuys();
 	}
 	std::cout << std::endl;
 	std::cout << "Tests done, now is time to fight." << std::endl;
 	std::cout << std::endl;
 	{
 		ScavTrap fighterOne("Guile");
-		ScavTrap fighterTwo("Tom");
+		FragTrap fighterTwo("Tom");
 
 		fighterOne.attack("Tom");
 		fighterTwo.takeDamage(fighterOne.getAttackDamage());
 		fighterTwo.attack("Guile");
 		fighterOne.takeDamage(fighterTwo.getAttackDamage());
 		fighterOne.guardGate();
-		fighterTwo.guardGate();
-		std::cout << "The fight ends cuz both fighters are in defensive mode..." << std::endl;
+		fighterTwo.highFivesGuys();
+		std::cout << "The fight ends cuz one fighter are in defensive mode "
+							<< "and the other guy are tryinh to make peace with a high five!" << std::endl;
 	}
 
   return EXIT_SUCCESS;
