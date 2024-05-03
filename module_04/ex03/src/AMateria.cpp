09bdbc4e0c6f5a7bff3ae32f2660339ee9e71e67
vde-frei <vde-frei@student.42sp.org.br>
2024-05-02 22:15:43 -0300

    fix: improved mainn function and Amaterial cpp

--- module_04/ex03/src/AMateria.cpp
+++ module_04/ex03/src/AMateria.cpp
@@ -1,27 +1,3 @@
-#include "AAnimal.hpp"
+#include "AMateria.hpp"
 #include <iostream>
 
-AAnimal::AAnimal(void) : _type("Generic AAnimal") {
-  std::cout << "Generic Aanimal constructor called." << std::endl;
-}
-
-AAnimal::~AAnimal(void) { std::cout << "AAnimal destructor called." << std::endl; }
-
-AAnimal::AAnimal(AAnimal const &rhs) {
-  std::cout << "AAnimal copy constructor called." << std::endl;
-  *this = rhs;
-}
-
-AAnimal &AAnimal::operator=(AAnimal const &rhs) {
-  std::cout << "AAnimal assignment operator called." << std::endl;
-  if (this != &rhs) {
-    this->_type = rhs._type;
-  }
-  return *this;
-}
-
-std::string AAnimal::getType(void) const { return this->_type; }
-
-void AAnimal::makeSound() const {
-  std::cout << "Generic AAnimal Sound" << std::endl;
-}
