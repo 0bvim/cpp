7063b0de434c82c31b91dc0d726d07d32f0b4415
vde-frei <vde-frei@student.42sp.org.br>
2024-05-04 22:03:00 +0300

    feat: ice cpp file implementation

---
+++ module_04/ex03/src/Ice.cpp
@@ -0,0 +1,28 @@
+#include "Ice.hpp"
+#include "AMateria.hpp"
+#include <iostream>
+
+Ice::Ice() : AMateria("Ice") {
+  std::cout << "Ice constructor called" << std::endl;
+}
+
+Ice::Ice(Ice const &rhs) {
+  std::cout << "Ice copy constructor called" << std::endl;
+  *this = rhs;
+}
+
+Ice &Ice::operator=(Ice const &rhs) {
+  std::cout << "Ice assignment constructor called" << std::endl;
+  if (this != &rhs) {
+    this->_type = rhs._type;
+  }
+  return *this;
+}
+
+Ice::~Ice() { std::cout << "Ice destructor called" << std::endl; }
+
+AMateria *Ice::clone() const { return new Ice(*this); }
+
+void Ice::use(ICharacter &target) {
+	std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
+}
