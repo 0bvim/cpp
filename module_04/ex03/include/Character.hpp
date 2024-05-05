50ec648e9fb427ecb265d4a2e74d936c91310192
vde-frei <vde-frei@student.42sp.org.br>
2024-05-05 10:44:23 -0300

    feat: implemented character cpp and hpp
    removed ICharacter cpp, don't needed

---
+++ module_04/ex03/include/Character.hpp
@@ -0,0 +1,25 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+class Character : public ICharacter {
+private:
+  std::string _name;
+  AMateria *_inventory[4];
+
+public:
+  Character();
+  Character(std::string const &name);
+  Character(Character const &rhs);
+  Character &operator=(Character const &rhs);
+  ~Character();
+
+  std::string const &getName() const;
+  void equip(AMateria *m);
+  void unequip(int idx);
+  void use(int idx, ICharacter &target);
+};
+
+#endif // !CHARACTER_HPP
