76e61e91c9ffe9a85f6abda95bd6fab3a6531bd9
vde-frei <vde-frei@student.42sp.org.br>
2024-05-05 11:30:14 -0300

    feat: implemented materia source cpp file

---
+++ module_04/ex03/src/MateriaSource.cpp
@@ -0,0 +1,52 @@
+#include "AMateria.hpp"
+#include "MateriaSouce.hpp"
+#include <iostream>
+
+MateriaSource::MateriaSource() {
+  std::cout << "MateriaSource constructor called." << std::endl;
+  for (int i = 0; i < 4; ++i) {
+    _learnedMaterias[i] = NULL;
+  }
+}
+
+MateriaSource::~MateriaSource() {
+  std::cout << "MateriaSource destructor called." << std::endl;
+  for (int i = 0; i < 4; ++i) {
+    delete _learnedMaterias[i];
+  }
+}
+
+MateriaSource::MateriaSource(MateriaSource const &rhs) {
+  std::cout << "MateriaSource copy constructor called." << std::endl;
+  *this = rhs;
+}
+
+MateriaSource &MateriaSource::operator=(MateriaSource const &rhs) {
+  if (this != &rhs) {
+    for (int i = 0; i < 4; ++i) {
+      if (rhs._learnedMaterias[i]) {
+        this->_learnedMaterias[i] = rhs._learnedMaterias[i]->clone();
+      } else {
+        this->_learnedMaterias[i] = NULL;
+      }
+    }
+  }
+  return *this;
+}
+
+void MateriaSource::learnedMateria(AMateria *m) {
+  for (int i = 0; i < 4; ++i) {
+    if (_learnedMaterias[i] == NULL) {
+      _learnedMaterias[i] = m;
+      break;
+    }
+  }
+}
+
+AMateria *MateriaSource::createMateria(std::string const &type) {
+  for (int i = 0; i < 4; ++i) {
+    if (_learnedMaterias[i] != NULL && _learnedMaterias[i]->getType() == type)
+      return _learnedMaterias[i]->clone();
+  }
+	return NULL;
+}
