b17bb066c270593e3b6a9cb6834aa24776fa42f6
vde-frei <vde-frei@student.42sp.org.br>
2024-05-05 09:09:54 -0300

    feat: implemented cure cpp file

---
+++ module_04/ex03/src/Cure.cpp
@@ -0,0 +1,29 @@
+#include "Cure.hpp"
+#include "ICharacter.hpp"
+#include <iostream>
+
+Cure::Cure() : AMateria("Cure") {
+  std::cout << "Cure constructor called" << std::endl;
+}
+
+Cure::Cure(Cure const &rhs) {
+  std::cout << "Cure copy constructor called" << std::endl;
+  *this = rhs;
+}
+
+Cure &Cure::operator=(Cure const &rhs) {
+  std::cout << "Cure assignment constructor called" << std::endl;
+  if (this != &rhs) {
+    this->_type = rhs._type;
+  }
+  return *this;
+}
+
+Cure::~Cure() { std::cout << "Cure destructor called" << std::endl; }
+
+AMateria *Cure::clone() const { return new Cure(*this); }
+
+void Cure::use(ICharacter &target) {
+  std::cout << "* shoots an Cure bolt at " << target.getName() << " *"
+            << std::endl;
+}
