10c54f09bb4631f253d426ae1ec31cdc3bfd4da9
vde-frei <vde-frei@student.42sp.org.br>
2024-05-05 11:14:02 -0300

    fix: cure message

--- module_04/ex03/src/Cure.cpp
+++ module_04/ex03/src/Cure.cpp
@@ -24,6 +24,6 @@ Cure::~Cure() { std::cout << "Cure destructor called" << std::endl; }
 AMateria *Cure::clone() const { return new Cure(*this); }
 
 void Cure::use(ICharacter &target) {
-  std::cout << "* shoots an Cure bolt at " << target.getName() << " *"
+  std::cout << "* heals " << target.getName() << "'s wounds *"
             << std::endl;
 }
