9fb521b57705bfde2a8ecf1de91979d45a5e2c33
nivicius <niviciusdev@gmail.com>
2024-05-05 22:37:13 -0300

    fixed ex02

--- module_04/ex02/src/Cat.cpp
+++ module_04/ex02/src/Cat.cpp
@@ -15,12 +15,14 @@ Cat::~Cat() {
 Cat::Cat(Cat const &rhs) : AAnimal(rhs) {
   std::cout << "Cat copy constructor called." << std::endl;
   *this = rhs;
+  _brain = new Brain();
 }
 
 Cat &Cat::operator=(Cat const &rhs) {
   std::cout << "Cat assignment operator called." << std::endl;
   if (this != &rhs) {
     this->_type = rhs._type;
+    *_brain = *(rhs._brain);
   }
   return *this;
 }
--- module_04/ex02/src/Dog.cpp
+++ module_04/ex02/src/Dog.cpp
@@ -15,12 +15,14 @@ Dog::~Dog() {
 Dog::Dog(Dog const &rhs) : AAnimal(rhs) {
   std::cout << "Dog copy constructor called." << std::endl;
   *this = rhs;
+	_brain = new Brain();
 }
 
 Dog &Dog::operator=(Dog const &rhs) {
   std::cout << "Dog assignment operator called." << std::endl;
   if (this != &rhs) {
     this->_type = rhs._type;
+    *_brain = *(rhs._brain);
   }
   return *this;
 }
