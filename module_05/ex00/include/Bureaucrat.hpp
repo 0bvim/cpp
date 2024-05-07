162b9a673f0dfa66bb82dcc96495f3a3bd0f8974
nivicius <niviciusdev@gmail.com>
2024-05-07 14:01:56 -0300

    feat: implemented header from Bureaucrat

---
+++ module_05/ex00/include/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <exception>
+#include <string>
+
+class Bureaucrat {
+private:
+  const std::string _name;
+  int _grade;
+
+public:
+  class GradeTooHighException : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "Grade is too high"; }
+  };
+
+  class GradeTooLowException : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "Grade is too low"; }
+  };
+
+  Bureaucrat(const std::string &name, int grade);
+  ~Bureaucrat();
+  Bureaucrat(Bureaucrat const &rhs);
+  Bureaucrat operator=(Bureaucrat const &rhs);
+
+  std::string getName() const;
+  int getGrade() const;
+
+  void incrementGrade();
+  void decrementGrade();
+};
+
+#endif // BUREAUCRAT_HPP
