1bc42c203edaf71f03cfb5a0a5d1440b14c4bb3b
vde-frei <vde-frei@student.42sp.org.br>
2024-05-09 23:05:42 -0300

    feat: included shrrubbery class

---
+++ module_05/ex02/include/ShrrubberyCreationForm.hpp
@@ -0,0 +1,8 @@
+#ifndef SHRRUBBERYCREATIONFORM_HPP
+#define SHRRUBBERYCREATIONFORM_HPP
+
+#include "AForm.hpp"
+
+class ShrrubberyCreationForm : public AForm {};
+
+#endif // !SHRRUBBERYCREATIONFROM_HPP
