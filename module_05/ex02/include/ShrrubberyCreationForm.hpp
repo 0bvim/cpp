a9f7c325c814f3e56eb07bc830a1747af436c8cd
nivicius <niviciusdev@gmail.com>
2024-05-10 02:07:02 -0300

    feat: implemented constructors.

--- module_05/ex02/include/ShrrubberyCreationForm.hpp
+++ module_05/ex02/include/ShrrubberyCreationForm.hpp
@@ -2,16 +2,44 @@
 #define SHRRUBBERYCREATIONFORM_HPP
 
 #include "AForm.hpp"
+#include <exception>
+#define ASCII_TRESS                                                            \
+  "               ,@@@@@@@,\n"                                                 \
+  "       ,,,.   ,@@@@@@/@@,  .oo8888o.\n"                                     \
+  "    ,&%%&%&&%,@@@@@/@@@@@@,8888\88/8o\n"                                    \
+  "   ,%&\%&&%&&%,@@@\@@@/@@@88\88888/88'\n"                                   \
+  "   %&&%&%&/%&&%@@\@@/ /@@@88888\88888'\n"                                   \
+  "   %&&%/ %&%%&&@@\ V /@@' `88\8 `/88'\n"                                    \
+  "   `&%\ ` /%&'    |.|        \ '|8'\n"                                      \
+  "       |o|        | |         | |\n"                                        \
+  "       |.|        | |         | |\n"                                        \
+  "    \\/ ._\//_/__/  ,\_//__\\/.  \_//__/_"
+
+#define SIGN_GRADE 145
+#define EXEC_GRADE 137
 
 class ShrrubberyCreationForm : public AForm {
-public:
-  ShrrubberyCreationForm(const std::string &target);
-  ShrrubberyCreationForm(ShrrubberyCreationForm const &rhs);
-  ShrrubberyCreationForm &operator=(ShrrubberyCreationForm const &rhs);
-  void execute(const Bureaucrat &executor) const;
-
 private:
   ShrrubberyCreationForm();
+  std::string _target;
+
+public:
+  ShrrubberyCreationForm(const std::string &target);
+  ~ShrrubberyCreationForm();
+  ShrrubberyCreationForm(ShrrubberyCreationForm const &rhs);
+  ShrrubberyCreationForm &operator=(ShrrubberyCreationForm const &rhs);
+
+  void execute(const Bureaucrat &executor) const;
+
+  class FileAlreadyExistException : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "File already exists."; }
+  };
+
+  class FailedToOpenException : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "Failed opening file"; }
+  };
 };
 
 #endif // !SHRRUBBERYCREATIONFROM_HPP
--- module_05/ex02/src/ShrrubberyCreationForm.cpp
+++ module_05/ex02/src/ShrrubberyCreationForm.cpp
@@ -1,1 +1,26 @@
 #include "ShrrubberyCreationForm.hpp"
+
+ShrrubberyCreationForm::ShrrubberyCreationForm()
+    : AForm("Shruberry Creation Form", SIGN_GRADE, EXEC_GRADE),
+      _target("unknow") {}
+
+ShrrubberyCreationForm::ShrrubberyCreationForm(const std::string &target)
+    : AForm("Shruberry Creation Form", SIGN_GRADE, EXEC_GRADE),
+      _target(target) {}
+
+ShrrubberyCreationForm::~ShrrubberyCreationForm() {}
+
+ShrrubberyCreationForm &
+ShrrubberyCreationForm::operator=(ShrrubberyCreationForm const &rhs) {
+  if (this != &rhs) {
+    AForm::operator=(rhs);
+    (std::string &)this->_target = rhs._target;
+  }
+  return *this;
+}
+
+ShrrubberyCreationForm::ShrrubberyCreationForm(
+    ShrrubberyCreationForm const &rhs)
+    : AForm("Shruberry Creation Form", SIGN_GRADE, EXEC_GRADE) {
+  *this = rhs;
+}
