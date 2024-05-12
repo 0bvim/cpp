1506866d4b6898165e9e58da825bfe11e457557c
vde-frei <vde-frei@student.42sp.org.br>
2024-05-12 07:00:28 -0300

    feat: started to implement converter

--- module_06/ex00/src/ScalarConverter.cpp
+++ module_06/ex00/src/ScalarConverter.cpp
@@ -8,10 +8,14 @@ ScalarConverter::~ScalarConverter() {}
 ScalarConverter::ScalarConverter(ScalarConverter const &rhs) { *this = rhs; }
 
 ScalarConverter &ScalarConverter::operator=(ScalarConverter const &rhs) {
-	(void)rhs;
-	return *this;
+  (void)rhs;
+  return *this;
 }
 
 static void converter(const std::string &str) {
+  char c;
+  int i;
+  float f;
+  double d;
 
 }
