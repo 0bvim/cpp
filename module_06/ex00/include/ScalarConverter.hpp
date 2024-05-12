187012c5efa30170c915b47cd91dc8b95a41ab4a
vde-frei <vde-frei@student.42sp.org.br>
2024-05-12 04:31:48 -0300

    doxygem

--- module_06/ex00/include/ScalarConverter.hpp
+++ module_06/ex00/include/ScalarConverter.hpp
@@ -3,14 +3,30 @@
 
 #include <string>
 
+/**
+ * @brief Class for converting scalar values.
+ * 
+ * This class provides functionality to convert scalar values represented as strings.
+ */
 class ScalarConverter {
 private:
-  ScalarConverter();
-  ScalarConverter(ScalarConverter const &rhs);
-  ScalarConverter &operator=(ScalarConverter const &rhs);
+  ScalarConverter(); // Private constructor to prevent instantiation
+  ScalarConverter(ScalarConverter const &rhs); // Private copy constructor
+  ScalarConverter &operator=(ScalarConverter const &rhs); // Private assignment operator
 
 public:
+  /**
+   * @brief Destructor.
+   */
   ~ScalarConverter();
+
+  /**
+   * @brief Convert the given string to a scalar value.
+   * 
+   * This function converts the given string to a scalar value and prints the result.
+   * 
+   * @param str The string to be converted.
+   */
   static void convert(const std::string &str);
 };
 
