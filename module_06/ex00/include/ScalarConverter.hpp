db961a9145cd566cb0cc7cd486e428a9bb5651e7
vde-frei <vde-frei@student.42sp.org.br>
2024-05-12 04:30:22 -0300

    feat: start to implemente cpp file

--- module_06/ex00/include/ScalarConverter.hpp
+++ module_06/ex00/include/ScalarConverter.hpp
@@ -1,6 +1,17 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#include <string>
 
+class ScalarConverter {
+private:
+  ScalarConverter();
+  ScalarConverter(ScalarConverter const &rhs);
+  ScalarConverter &operator=(ScalarConverter const &rhs);
 
-#endif  // SCALARCONVERTER_HPP
+public:
+  ~ScalarConverter();
+  static void convert(const std::string &str);
+};
+
+#endif // SCALARCONVERTER_HPP
--- module_06/ex00/src/ScalarConverter.cpp
+++ module_06/ex00/src/ScalarConverter.cpp
@@ -1,1 +1,17 @@
 #include "ScalarConverter.hpp"
+#include "Defines.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::~ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(ScalarConverter const &rhs) { *this = rhs; }
+
+ScalarConverter &ScalarConverter::operator=(ScalarConverter const &rhs) {
+	(void)rhs;
+	return *this;
+}
+
+static void converter(const std::string &str) {
+
+}
