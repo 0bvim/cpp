3a2778e6fa65dd3de78b532502f58877b02ccdbb
nivicius <niviciusdev@gmail.com>
2024-05-11 23:48:57 -0300

    fix: indentation

--- module_05/ex03/src/main.cpp
+++ module_05/ex03/src/main.cpp
@@ -1,66 +1,58 @@
-#include "Bureaucrat.hpp"
 #include "AForm.hpp"
-#include "ShrubberyCreationForm.hpp"
-#include <iostream>
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
 #include "Intern.hpp"
+#include <iostream>
 
-int main()
-{
-    Intern someRandomIntern;
-    AForm* tmp;
+int main() {
+  Intern someRandomIntern;
+  AForm *tmp;
 
-    std::cout << GREEN("\nROBOTOMY REQUEST") << std::endl;
-    try {
-        tmp = someRandomIntern.makeForm("robotomy request", "Bender");
-        if(tmp) {
-            std::cout << GREEN("Form created successfully!") << std::endl;
-            delete tmp;
-        }
+  std::cout << GREEN("\nROBOTOMY REQUEST") << std::endl;
+  try {
+    tmp = someRandomIntern.makeForm("robotomy request", "Bender");
+    if (tmp) {
+      std::cout << GREEN("Form created successfully!") << std::endl;
+      delete tmp;
     }
-    catch (Intern::InvalidFormException &e) {
-        std::cout << YELLOW("Form not found!") << std::endl;
-    }
+  } catch (Intern::InvalidFormException &e) {
+    std::cout << YELLOW("Form not found!") << std::endl;
+  }
 
-    std::cout << GREEN("\nSHRUBBERY CREATION") << std::endl;
-    try {
-        tmp = someRandomIntern.makeForm("shrubbery creation", "Downtown");
-        if(tmp) {
-            std::cout << GREEN("Form created successfully!") << std::endl;
-            delete tmp;
-        }
+  std::cout << GREEN("\nSHRUBBERY CREATION") << std::endl;
+  try {
+    tmp = someRandomIntern.makeForm("shrubbery creation", "Downtown");
+    if (tmp) {
+      std::cout << GREEN("Form created successfully!") << std::endl;
+      delete tmp;
     }
-    catch (Intern::InvalidFormException &e) {
-        std::cout << YELLOW("Form not found!") << std::endl;
-    }
+  } catch (Intern::InvalidFormException &e) {
+    std::cout << YELLOW("Form not found!") << std::endl;
+  }
 
-    std::cout << GREEN("\nPRESIDENTIAL PARDON") << std::endl;
-    try {
-        tmp = someRandomIntern.makeForm("presidential pardon",
-                                         "corrupt politician");
-        if(tmp) {
-            std::cout << GREEN("Form created successfully!") << std::endl;
-            delete tmp;
-        }
+  std::cout << GREEN("\nPRESIDENTIAL PARDON") << std::endl;
+  try {
+    tmp =
+        someRandomIntern.makeForm("presidential pardon", "corrupt politician");
+    if (tmp) {
+      std::cout << GREEN("Form created successfully!") << std::endl;
+      delete tmp;
     }
-    catch (Intern::InvalidFormException &e) {
-        std::cout << YELLOW("Form not found!") << std::endl;
-    }
+  } catch (Intern::InvalidFormException &e) {
+    std::cout << YELLOW("Form not found!") << std::endl;
+  }
 
-    std::cout << GREEN("\nTAX EXEMPTION") << std::endl;
-    try {
-        tmp = someRandomIntern.makeForm("tax exemption", "Bruno Moretti");
-        if(tmp) {
-            std::cout << GREEN("Form created successfully!") << std::endl;
-            delete tmp;
-        }
+  std::cout << GREEN("\nTAX EXEMPTION") << std::endl;
+  try {
+    tmp = someRandomIntern.makeForm("tax exemption", "Bruno Moretti");
+    if (tmp) {
+      std::cout << GREEN("Form created successfully!") << std::endl;
+      delete tmp;
     }
-    catch (Intern::InvalidFormException &e) {
-        std::cout << YELLOW("Form not found!") << std::endl;
-    }
+  } catch (Intern::InvalidFormException &e) {
+    std::cout << YELLOW("Form not found!") << std::endl;
+  }
 
-    std::cout << std::endl;
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
