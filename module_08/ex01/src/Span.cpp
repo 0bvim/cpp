ce3fa8aceab460d91fd6aab2fa97561c4f4dc993
vde-frei <vde-frei@student.42sp.org.br>
2024-05-15 03:27:52 -0300

    feat: shortestSpan

--- module_08/ex01/src/Span.cpp
+++ module_08/ex01/src/Span.cpp
@@ -12,24 +12,30 @@ Span::~Span() {}
 Span::Span(const Span &rhs) : _numbers(rhs._numbers), _maxSize(rhs._maxSize) {}
 
 Span &Span::operator(const Span &rhs) {
-	if (this != &rhs) {
-		_numbers = rhs._numbers;
-		_maxSize = rhs._maxSize;
-	}
-	return *this;
+  if (this != &rhs) {
+    _numbers = rhs._numbers;
+    _maxSize = rhs._maxSize;
+  }
+  return *this;
 }
 
 void Span::addNumber(int number) {
-	if (_numbers.size() >=  _maxSize)
-		throw std::overflow_error("Cannot add more numbers. Span is full.");
-	_numbers.push_back(number);
+  if (_numbers.size() >= _maxSize)
+    throw std::overflow_error("Cannot add more numbers. Span is full.");
+  _numbers.push_back(number);
 }
 
 int Span::shortestSpan() const {
-	if (_numbers.size() < 2)
-		throw std::logic_error("Not enough numbers to find a span.");
-	std::std::vector<int> sorted(_numbers);
-	std::sort(sorted.begin(), sorted.end());
-	int minSpan = std::numeric_limits<int>::max();
-	
+  if (_numbers.size() < 2)
+    throw std::logic_error("Not enough numbers to find a span.");
+  std::vector<int> sorted(_numbers);
+  std::sort(sorted.begin(), sorted.end());
+  int minSpan = std::numeric_limits<int>::max();
+  for (size_t i = 1; i < sorted.size(); ++i) {
+    int span = sorted[i] - sorted[i - 1];
+    if (span < minSpan) {
+      minSpan = span;
+    }
+  }
+  return minSpan;
 }
