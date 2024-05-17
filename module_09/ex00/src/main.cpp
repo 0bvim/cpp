f29ec3e314b66d5abb32e8ee5a6b4dea5931aa6a
nivicius <niviciusdev@gmail.com>
2024-05-16 21:04:12 -0300

    feat: inser a function to show correct way to use program

--- module_09/ex00/src/main.cpp
+++ module_09/ex00/src/main.cpp
@@ -6,6 +6,11 @@
 #include <stdexcept>
 #include <string>
 
+static void usage(void) {
+	NL;
+  std::cerr << MAGENTA("Usage: ./bin/btc <filename>") << std::endl;
+}
+
 void start_program(char *av) {
 
   Exchange btc;
@@ -20,6 +25,7 @@ void start_program(char *av) {
 int main(int ac, char **av) {
   if (ac == 2)
     start_program(av[1]);
+  usage();
 
   return 0;
 }
